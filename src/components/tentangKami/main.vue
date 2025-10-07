7b9a85ef4477040d7874ae2d62acf4135f025660
savri <savahp123@gmail.com>
2025-10-07 10:44:45 +0700

    main tentang kami

--- src/components/tentangKami/main.vue
+++ src/components/tentangKami/main.vue
@@ -1,10 +1,61 @@
 <script setup></script>
 
 <template>
-    <!-- ngoding html e ng kene -->
-
-     <h1>Ini section main kami</h1>
+    <div class="main">
+        <div class="left-container">
+            <h1 class="h1">Bukan Sekedar <span style="color: #D4A300;">Keripik <br> Biasa.</span> Ini adalah <br><span style="color: #D4A300;">Tetato Chips.</span></h1>
+            <p> Wujud nyata dari semangat kami <br> untuk menciptakan snack kentang premium <br> yang menawarkan pengalaman kriuk tak terlupakan. <br>Didirikan di Surabaya pada maret 2024.</p>
+            <div class="button">
+                <button><a href="">Ikuti Kami  ></a></button>
+            </div>
+        </div>
+        <div class="right-container">
+            <img src="/chipsproduk.png" alt="">
+        </div>
+    </div>
 </template>
 
 
-<style scoped></style>
+<style scoped>
+
+    .main {
+        display: flex;
+        justify-content: space-evenly;
+        margin-top: 130px;
+    }
+
+    .h1 {
+        font-family: 'dynapuff';
+        font-size: 40px; 
+    }
+
+    .button button {
+        padding: 5px;
+        width: 250px;
+        font-size: 20px;
+        font-family: 'poppins', sans-serif;
+        font-weight: 700;
+        background-color: #FFC300;
+        border: #FFC300;
+        border-radius: 15px;
+        margin-top: 10px;
+    }
+
+    .button button a {
+        text-decoration: none;
+        color: white;
+        background-color: #FFC300;
+    }
+
+    p {
+        font-size: 18px;
+        font-family: 'poppins', sans-serif;
+        font-weight: 400;
+        margin-top: 20px;
+    }
+
+    .right-container img {
+        width: 350px;
+        height: 350px;
+    }
+</style>
