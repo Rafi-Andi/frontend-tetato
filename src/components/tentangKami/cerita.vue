8cc30b1446e981b0fb7a4f6bfaa36429aa9a0010
savri <savahp123@gmail.com>
2025-10-08 12:42:44 +0700

    tentang kami

--- src/components/tentangKami/cerita.vue
+++ src/components/tentangKami/cerita.vue
@@ -1,10 +1,48 @@
 <script setup></script>
 
 <template>
-    <!-- ngoding html e ng kene -->
-
-     <h1>Ini cerita tentang kami</h1>
+    <div class="container">
+        <div class="judul">
+            <h1>Cerita Kami</h1>
+        </div>
+        <div class="content">
+            <div class="left-container">
+                <img src="/cerita.png" alt="" width="400px">
+            </div>
+            <div class="right-container">
+                <h1>Berawal Dari Ide <br> Sedeharna</h1>
+                <p>Cikal bakal Tetato Chips <br> bermula dari ide <br> sederhana: menciptakan <br> camilan kentang yang <br>unik, renyah berbeda, <br> dan penuh cita rasa <br>yang disukai anak muda</p>
+                <button>
+                    <a href="Lihat Produk"></a>
+                </button>
+            </div>
+        </div>
+    </div>
 </template>
 
 
-<style scoped></style>
+<style scoped>
+    .container {
+        margin-top: 70px;
+    }
+
+    .judul h1 {
+        text-align: center;
+        font-size: 45px;
+        color: #d4a300;
+        font-family: 'dynapuff';
+    }
+
+    .content {
+        position: relative;
+    }
+
+    .left-container {
+        position: absolute;
+    }
+
+    .right-container {
+        position: absolute; 
+        font-family: 'poppins';    
+    }
+</style>
